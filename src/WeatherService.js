f25961b8e9e16dd2c61a182a05cf829f3e38d971
vinay chowdary <vinaychowdary@vinays-MacBook-Air.local>
2025-05-27 22:58:10 +0530

    Use .env for API key and secure config

--- src/WeatherService.js
+++ src/WeatherService.js
@@ -2,7 +2,8 @@
 
 import axios from 'axios';
 
-const API_KEY = 'bdf12dcda8ac529d548dca4d125f9025';
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
 const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
 export const getCurrentWeather = async (city) => {
